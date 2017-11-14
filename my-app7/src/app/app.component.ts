31d673be3974030938c34c874242fc4dde949492
Nathan Pickard <nathan.pickard87@gmail.com>
2017-11-13 22:07:11 -0800

    Working with Angular CLI json file and app generation flags

---
+++ my-app7/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+const num = 7;
+
+@Component({
+  selector: 'jp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'jp works! of course';
+}
